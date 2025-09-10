ef0cee11c798ec05a4c5ac06ff347208ae65c636
THIMAP20 <thimashapramudi@gmail.com>
2025-09-10 16:31:51 +0530

    initial codes

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ st.write("""
 # App Marketing
 Hello *world!*
 """)
+import streamlit as st
+import pandas as pd
+import joblib
+import altair as alt
+
+# ----------------------------
+# Load trained model
+# ----------------------------
+model = joblib.load("predict_new_campaign_.pkl")
+
+st.set_page_config(page_title="Predict campaign success", layout="wide")
+
+st.title("📊 Predict campaign success")
+st.markdown("Predict success of a campaign based on its settings.")
