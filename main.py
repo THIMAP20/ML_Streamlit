1bac1764f95c02e6d12482271c301e2cf05214cd
Achintha Balasooriya <36558525+AchinthaBala@users.noreply.github.com>
2025-09-10 21:39:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import altair as alt
 # ----------------------------
 # Load trained model
 # ----------------------------
-import joblib
-
-try:
-    model = joblib.load("predict_new_campaign.pkl")
-    print("✅ Model loaded successfully")
-except ModuleNotFoundError as e:
-    print("❌ Missing module:", e)
-
 st.set_page_config(page_title="Predict campaign success", layout="wide")
 
 st.title("📊 Predict campaign success")
