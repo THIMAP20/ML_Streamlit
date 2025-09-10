f8cc578aac9d8f0c8c6bb5f1e61c94a6b86a2db5
Achintha Balasooriya <36558525+AchinthaBala@users.noreply.github.com>
2025-09-10 21:42:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import streamlit as st
-import pandas as pd
-import joblib
-import altair as alt
-# ----------------------------
-# Load trained model
-# ----------------------------
+
 st.set_page_config(page_title="Predict campaign success", layout="wide")
 
-# st.title("📊 Predict campaign success")
-# st.markdown("Predict success of a campaign based on its settings.")
+
+
