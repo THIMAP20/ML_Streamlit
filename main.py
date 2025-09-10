f3699a18615d8661c4e41e7c52e66f54069abd08
THIMAP20 <thimashapramudi@gmail.com>
2025-09-10 22:20:05 +0530

    New files

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
- 
+import joblib
+import altair as alt
+
+model = joblib.load("predict_new_campaign.pkl")
+
 st.write("""
 # My first app
 Hello *world!*
